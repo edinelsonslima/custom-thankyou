60f097ba10377dfd285743341d51b184f9e7673f
Edinelson Lima <edinelsonlima86@gmail.com>
2024-10-15 14:20:47 -0300

    feat: add local redirect url

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const thankyouScript = referrerIndex === -1 ? thankyou.at(0) : thankyou.at(refer
 
 const search = new URLSearchParams(window.location.search)
 const upsell = search.get('upsell') ?? '38527'
+const local = search.get('local') ?? false
 
 bntRedirect.textContent = bntRedirect.textContent.replace('{{checkoutId}}', upsell)
 
@@ -26,5 +27,6 @@ script.setAttribute('src', thankyouScript);
 document.body.appendChild(script)
 
 bntRedirect.addEventListener('click', () => {
-  window.location.href = `${referrerCheckout}/${upsell}?u=1`
+  const url = (local ?? referrerCheckout).replace(/https?:\/\//g, '')
+  window.location.href = `//${url}/${upsell}?u=1`
 })
