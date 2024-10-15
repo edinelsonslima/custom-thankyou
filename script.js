0a95472575ecb0849c4a72a05d633b94dca083b7
Edinelson Lima <edinelsonlima86@gmail.com>
2024-10-15 14:04:56 -0300

    refactor: remove listener load

--- script.js
+++ script.js
@@ -16,19 +16,15 @@ const referrerIndex = referrers.findIndex((v) => v === document.referrer);
 const referrerCheckout = referrerIndex === -1 ? referrers.at(0) : referrers.at(referrerIndex)
 const thankyouScript = referrerIndex === -1 ? thankyou.at(0) : thankyou.at(referrerIndex)
 
-const checkoutId = new URL(referrerCheckout).pathname.replace('/', '') || '38528'
+const search = new URLSearchParams(window.location.search)
+const upsell = search.get('upsell') ?? '38527'
+
+bntRedirect.textContent = bntRedirect.textContent.replace('{{checkoutId}}', upsell)
+
+const script = document.createElement('script')
+script.setAttribute('src', thankyouScript);
+document.body.appendChild(script)
 
 bntRedirect.addEventListener('click', () => {
-  const search = new URLSearchParams(window.location.search)
-  const upsell = search.get('upsell') ?? '38527'
-
   window.location.href = `${referrerCheckout}/${upsell}?u=1`
 })
-
-window.addEventListener('load', () => {
-  bntRedirect.textContent = bntRedirect.textContent.replace('{{checkoutId}}', checkoutId)
-
-  const script = document.createElement('script')
-  script.setAttribute('src', thankyouScript);
-  document.body.appendChild(script)
-}, { once: true })
